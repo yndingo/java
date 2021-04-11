6bd2d1169066e141f1de2295cd93b7e8ef807d8c
yndingo <80397258+yndingo@users.noreply.github.com>
2021-04-11 21:34:21 +0500

    Add files via upload

--- ngp/JavaApplication1.java
+++ ngp/JavaApplication1.java
@@ -7,12 +7,27 @@
 //во второй строке ввести разделенные пробелом массив чисел величину которого
 //указали в 1й строке
 //Ввод данных переорганизован через файл с данными, строки файлы указаны выше
+//Программа должна принимать на входе файлы из папки «input» и размещать результаты в папке «output» 
 package javaapplication1;
 import java.util.Arrays;
 import java.util.Scanner;
 import java.io.FileNotFoundException;
 import java.io.File;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+
 public class JavaApplication1 {
+    
+    public static void listFilesForFolder(final File folder) {
+        for (final File fileEntry : folder.listFiles()) {
+            if (fileEntry.isDirectory()) {
+                listFilesForFolder(fileEntry);
+            } else {
+                System.out.println(fileEntry.getName());
+            }
+        }
+    }
+
     static int notDivisibleSubset(int arr[], int N, int K)
     {
         // Массив для храненения частоты деления по модуля 
@@ -37,7 +52,7 @@ public class JavaApplication1 {
       
         return res;
     }
-    public static void main(String[] args)
+    public static void main(String[] args) throws UnsupportedEncodingException
     {     
         //int arr[] = {3, 7, 2, 9, 1, 11, 5};
         //int N = arr.length;
@@ -48,27 +63,46 @@ public class JavaApplication1 {
 //        int arr[] = new int[N];
 //        for (int i = 0; i< N;i++)
 //            arr[i] = sc.nextInt();
-        File file = new File("input16.txt");
-        try {
-            Scanner sc = new Scanner(file);
-            int N = 0, K = 0;
-            int arr[] = null;
-            while (sc.hasNextLine()) {
-                N = sc.nextInt();
-                K = sc.nextInt();
-                arr = new int[N];
-                for (int i = 0; i< N;i++)
-                    arr[i] = sc.nextInt();
-            }            
-            System.out.println(notDivisibleSubset(arr, N, K));
-            sc.close();
-        } 
-        catch (FileNotFoundException e) {
-            e.printStackTrace();
+        //final File folder = new File("D:\\Мои документы\\NetBeansProjects\\JavaApplication1\\input");
+        //listFilesForFolder(folder);
+        
+        //путь к папке input
+        File folderIn = new File("D:\\Мои документы\\NetBeansProjects\\JavaApplication1\\input");
+        File[] listOfFilesIn = folderIn.listFiles();
+        //путь к папке output
+        String fileOut = "D:\\Мои документы\\NetBeansProjects\\JavaApplication1\\output\\";
+
+        for (File file : listOfFilesIn) {
+            if (file.isFile()) {
+                String fileName = file.getName();
+                System.out.println(fileName);
+                try {
+                    Scanner sc = new Scanner(file);
+                    int N = 0, K = 0;
+                    int arr[] = null;
+                    while (sc.hasNextLine()) {
+                        N = sc.nextInt();
+                        K = sc.nextInt();
+                        arr = new int[N];
+                        for (int j = 0; j< N;j++)
+                            arr[j] = sc.nextInt();
+                    }            
+                    System.out.println(notDivisibleSubset(arr, N, K));
+                    sc.close();
+                    PrintWriter writer = new PrintWriter(fileOut + fileName.substring(fileName.length()-6), "UTF-8");
+                    writer.println( notDivisibleSubset(arr, N, K) );
+                    writer.close();
+                }
+                catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+                catch (NullPointerException e) {
+                    e.printStackTrace();
+                }
+            }
         }
-        catch (NullPointerException e) {
-            e.printStackTrace();
-        }
-    }
+        
+        
     
+    }
 }
