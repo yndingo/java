07276f357980616e5b99247931c08d9b76e13fea
yndingo <80397258+yndingo@users.noreply.github.com>
2021-05-04 11:07:28 +0500

    Add files via upload

---
+++ Perfomance Lab/HomeWork/Task1/Multiplication.java
@@ -0,0 +1,16 @@
+/*Напишите приложение Multiplication, которое после запуска выводит на экран таблицу умножения чисел до 10 и завершает работу. 
+Числа разделить пробелом. (Вариант “со звёздочкой”: выровняйте их нужным количеством пробелов).
+*/
+package Task1;
+
+public class Multiplication {
+    
+    public static void main(String args[]){
+        for (int i = 1; i < 11; i++){
+            System.out.print("\n");
+            if (i > 1) System.out.print("        ");
+            for (int j = 1; j < 11; j++)
+                System.out.print(i*j + " ");
+        }
+    }
+}
