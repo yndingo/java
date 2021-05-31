2828db91cfb7c09ac27658c8919fddb81b95758a
yndingo <yndingo@gmail.com>
2021-05-31 17:22:21 +0500

    обновление

--- Perfomance Lab/RemoteWork/Task5/src/main/java/ru/pflb/Main.java
+++ Perfomance Lab/RemoteWork/Task5/src/main/java/ru/pflb/Main.java
@@ -1,26 +1,25 @@
 package ru.pflb;
 
-import javax.print.attribute.standard.*;
 import ru.pflb.mq.dummy.exception.DummyException;
 import ru.pflb.mq.dummy.implementation.*;
-import ru.pflb.mq.dummy.interfaces.*;
 import java.util.Queue;
 import java.util.LinkedList;
 
 public class Main {
-    public static void main(String[] args) throws DummyException {
+    
+    public static void main(String[] args) throws DummyException{
         //System.out.println("Hello");
         Queue <String> queue = new LinkedList<>();
-        queue.offer("Ð Ð°Ð·");
-        queue.offer("Ð”Ð²Ð°");
-        queue.offer("Ð¢Ñ€Ð¸");
+        queue.offer("Ðàç");
+        queue.offer("Äâà");
+        queue.offer("Òðè");
         ConnectionImpl ConImpl = new ConnectionImpl();
         ConImpl.start();
         SessionImpl SessImpl = (SessionImpl)(ConImpl.createSession(true));
         DestinationImpl DestImpl = (DestinationImpl)(SessImpl.createDestination("test"));
         ProducerImpl ProdImpl = (ProducerImpl)(SessImpl.createProducer(DestImpl));
         //ProdImpl.send("World");
-
+        
         String str;
         while ( (str = queue.poll()) != null){
             //System.out.println(str);
@@ -30,10 +29,11 @@ public class Main {
             }
             catch(InterruptedException e){
                 System.out.println(e);
-            }
+            }  
         }
 
         SessImpl.close();
         ConImpl.close();
     }
+    
 }
