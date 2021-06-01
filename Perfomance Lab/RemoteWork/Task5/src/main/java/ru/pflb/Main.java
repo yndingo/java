956de01f15ce144fcfcc1f7296a0b0c3127ceb19
yndingo <yndingo@gmail.com>
2021-06-01 15:01:35 +0500

    обновление

--- Perfomance Lab/RemoteWork/Task5/src/main/java/ru/pflb/Main.java
+++ Perfomance Lab/RemoteWork/Task5/src/main/java/ru/pflb/Main.java
@@ -1,39 +1,37 @@
 package ru.pflb;
 
 import ru.pflb.mq.dummy.exception.DummyException;
-import ru.pflb.mq.dummy.implementation.*;
+import ru.pflb.mq.dummy.implementation.ConnectionImpl;
+import ru.pflb.mq.dummy.interfaces.*;
 import java.util.Queue;
 import java.util.LinkedList;
 
 public class Main {
     
     public static void main(String[] args) throws DummyException{
-        //System.out.println("Hello");
         Queue <String> queue = new LinkedList<>();
         queue.offer("Раз");
         queue.offer("Два");
         queue.offer("Три");
-        ConnectionImpl ConImpl = new ConnectionImpl();
-        ConImpl.start();
-        SessionImpl SessImpl = (SessionImpl)(ConImpl.createSession(true));
-        DestinationImpl DestImpl = (DestinationImpl)(SessImpl.createDestination("test"));
-        ProducerImpl ProdImpl = (ProducerImpl)(SessImpl.createProducer(DestImpl));
-        //ProdImpl.send("World");
-        
-        String str;
-        while ( (str = queue.poll()) != null){
-            //System.out.println(str);
-            ProdImpl.send(str);
-            try{
-                Thread.sleep(2000L);
-            }
-            catch(InterruptedException e){
-                System.out.println(e);
-            }  
-        }
 
-        SessImpl.close();
-        ConImpl.close();
+        try(
+            Connection conn = new ConnectionImpl();//close
+            Session session = conn.createSession(true);//close
+                )
+        {
+            Destination dest = session.createDestination("test");
+            Producer prod = session.createProducer(dest);
+
+            String str;
+            while ( (str = queue.poll()) != null){
+                prod.send(str);
+                try{
+                    Thread.sleep(2000L);
+                }
+                catch(InterruptedException e){
+                    System.out.println(e);
+                }  
+            }
+        }
     }
-    
 }
