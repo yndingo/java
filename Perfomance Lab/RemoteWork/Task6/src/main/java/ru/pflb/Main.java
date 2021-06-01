d06ddbacd989e49d78dd86c9c8e2d061cfd157e7
yndingo <yndingo@gmail.com>
2021-06-01 21:07:43 +0500

    Обвноление

--- Perfomance Lab/RemoteWork/Task6/src/main/java/ru/pflb/Main.java
+++ Perfomance Lab/RemoteWork/Task6/src/main/java/ru/pflb/Main.java
@@ -2,21 +2,73 @@ package ru.pflb;
 
 import ru.pflb.mq.dummy.exception.DummyException;
 import ru.pflb.mq.dummy.implementation.*;
+import ru.pflb.mq.dummy.interfaces.*;
 import java.util.Queue;
 import java.util.LinkedList;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
-public class Main {
+import org.jnativehook.GlobalScreen;
+import org.jnativehook.NativeHookException;
+import org.jnativehook.keyboard.NativeKeyEvent;
+import org.jnativehook.keyboard.NativeKeyListener;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+class GlobalKeyListenerExample implements NativeKeyListener {
+    static boolean exit = false;  
+    public void nativeKeyPressed(NativeKeyEvent e) {
+            //System.out.println("Key Pressed: " + NativeKeyEvent.getKeyText(e.getKeyCode()));
+
+            if (e.getKeyCode() == NativeKeyEvent.VC_ESCAPE) {
+                    try {
+                            System.out.println("Нажата кнопка ESC. Завершаем работу.");
+                            exit = true;
+                            GlobalScreen.unregisterNativeHook();
+                    } catch (NativeHookException nativeHookException) {
+                            nativeHookException.printStackTrace();
+                    }
+            }
+    }
+
+    public void nativeKeyReleased(NativeKeyEvent e) {
+            //System.out.println("Key Released: " + NativeKeyEvent.getKeyText(e.getKeyCode()));
+    }
+
+    public void nativeKeyTyped(NativeKeyEvent e) {
+            //System.out.println("Key Typed: " + e.getKeyText(e.getKeyCode()));
+    }
+}
+
+public class Main {      
+    
     public static void main(String[] args) throws DummyException{
+        
+         // Get the logger for "org.jnativehook" and set the level to warning.
+        Logger logger = Logger.getLogger(GlobalScreen.class.getPackage().getName());
+        logger.setLevel(Level.WARNING);
+
+        // Don't forget to disable the parent handlers.
+        logger.setUseParentHandlers(false);
+        try {
+			GlobalScreen.registerNativeHook();
+		}
+		catch (NativeHookException ex) {
+			System.err.println("There was a problem registering the native hook.");
+			System.err.println(ex.getMessage());
+
+			System.exit(1);
+		}
+
+        GlobalScreen.addNativeKeyListener(new GlobalKeyListenerExample());
 
         for (String s : args){
             System.out.println(s);
         }
         if (args.length == 0) {
-            System.out.println("РќРµ РЅР°Р№РґРµРЅРѕ РёРјСЏ С„Р°Р№Р»Р° РґР»СЏ Р°РЅР°Р»РёР·Р°");
+            System.out.println("Не найдено имя файла для анализа");
             return;
         }
         FileReader input = null;
@@ -27,48 +79,36 @@ public class Main {
             System.out.println(e);
             return;
         }
-        BufferedReader bufRead = new BufferedReader(input);
-        String myLine;
-        Queue <String> queue = new LinkedList<>();
-        try {
-            while ( (myLine = bufRead.readLine())!=null )
-                queue.offer(myLine);
-        }
-        catch(Exception e){System.out.println(e);}
 
-        ConnectionImpl ConImpl = new ConnectionImpl();
-        ConImpl.start();
-        SessionImpl SessImpl = (SessionImpl)(ConImpl.createSession(true));
-        DestinationImpl DestImpl = (DestinationImpl)(SessImpl.createDestination("test"));
-        ProducerImpl ProdImpl = (ProducerImpl)(SessImpl.createProducer(DestImpl));
-        //ProdImpl.send("World");
-
-        Runtime.getRuntime().addShutdownHook(new Thread() {
-            public void run() {
-                SessImpl.close();
-                ConImpl.close();
-                //System.out.println("РћР±СЂР°Р±РѕС‚РєР° РїСЂРµСЂС‹РІР°РЅРёСЏ, РѕСЃС‚Р°РЅР°РІР»РёРІР°РµРј СЃРµСЃСЃРёСЋ Рё СЃРѕРµРґРёРЅРµРЅРёРµ");
+        try(
+            BufferedReader bufRead = new BufferedReader(input);
+            Connection conn = new ConnectionImpl();//close
+            Session session = conn.createSession(true);//close
+                )
+        {
+            Queue <String> queue = new LinkedList<>();
+            String str;
+            try {
+                while ( (str = bufRead.readLine())!=null )
+                    queue.offer(str);
             }
-        });
+            catch(Exception e){System.out.println(e);}
+        
+            Destination dest = session.createDestination("test");
+            Producer prod = session.createProducer(dest);
+            System.out.println("Для остановки программы нажмите ESC.");
 
-        String str;
-        while ( (str = queue.poll()) != null){
-            //System.out.println(str);
-            //s = sc.nextLine();
-            //if (sc.nextLine().isEmpty()) break;
-            queue.offer(str);
-            ProdImpl.send(str);
-            try{
-                Thread.sleep(2000L);
-            }
-            catch(InterruptedException e){
-                System.out.println(e);
+            while (true){
+                if (GlobalKeyListenerExample.exit) break;
+                str = queue.poll();
+                queue.offer(str);
+                prod.send(str);
+                try{
+                    Thread.sleep(2000L);
+                }
+                catch(InterruptedException e){ System.out.println(e); }
             }
         }
-        SessImpl.close();
-        ConImpl.close();
-
-
+        catch(IOException e){ System.out.println(e); }
     }
-
 }
