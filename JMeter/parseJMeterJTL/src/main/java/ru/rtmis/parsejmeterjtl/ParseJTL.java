f936b5662ad2a54812a4eb99cdd1a5965d67246c
yndingo <80397258+yndingo@users.noreply.github.com>
2021-10-21 09:25:35 +0500

    Add files via upload

--- JMeter/parseJMeterJTL/src/main/java/ru/rtmis/parsejmeterjtl/ParseJTL.java
+++ JMeter/parseJMeterJTL/src/main/java/ru/rtmis/parsejmeterjtl/ParseJTL.java
@@ -18,25 +18,37 @@ import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.LineIterator;
 
 public class ParseJTL {
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException{
         System.out.println("begin parsing");      
 
-        try {
-            usingLineIterator();
-        } catch(IOException e) {
-            System.out.println("Исключение в usingLineIterator");
-            System.out.println(e.getMessage());
-        }
-   }
-   public static void usingLineIterator() throws IOException {
        //file objects
-       File fileIN        = FileUtils.getFile("c:/!d/testResults.jtl");
-       File fileOUT       = FileUtils.getFile("c:/!d/ramp.jtl");
+       //File fileIN        = FileUtils.getFile("c:/!d/testResults.jtl");
+       //File fileOUT       = FileUtils.getFile("c:/!d/ramp.jtl");
+       if (args.length == 0) {
+           System.out.println("Данные вводить через параметры:");
+           System.out.println("аргумент 1 - файл для ввода например: c:/!d/testResults.jtl");
+           System.out.println("аргумент 2 - файл для ввода например: c:/!d/ramp.jtl");
+           System.out.println("аргумент 3 - номер полки например: 2");
+           System.out.println("аргумент 4 - время начала полки: 2021-10-14 17:21:00");
+           System.out.println("аргумент 5 - время конца  полки: 2021-10-15 05:52:00");         
+           
+           System.exit(0);
+       }
+       String fileNameOUT = args[1].substring(0, args[1].length()-4) + args[2] + args[1].substring(args[1].length()-4);
+       System.out.println("Начата работа с " + args[0] + " Вывод будет в " + fileNameOUT);
+       System.out.println("Полка началась в " + args[3] + " Полка закончилась в " + args[4]);
+       
+       File fileIN        = FileUtils.getFile(args[0]);
+       //File fileOUT       = FileUtils.getFile(args[1]+args[2]);
+       File fileOUT       = FileUtils.getFile(fileNameOUT);    
+       
        PrintWriter writer = new PrintWriter(fileOUT, "UTF-8");     
        // Format data and time to analyze
        DateTimeFormatter inputFormatWithMS = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-       LocalDateTime rampBegin             = LocalDateTime.parse("2021-10-14 17:21:00", inputFormatWithMS);
-       LocalDateTime rampEnd               = LocalDateTime.parse("2021-10-15 05:52:00", inputFormatWithMS);
+       //LocalDateTime rampBegin             = LocalDateTime.parse("2021-10-14 17:21:00", inputFormatWithMS);
+       //LocalDateTime rampEnd               = LocalDateTime.parse("2021-10-15 05:52:00", inputFormatWithMS);
+       LocalDateTime rampBegin             = LocalDateTime.parse(args[3], inputFormatWithMS);
+       LocalDateTime rampEnd               = LocalDateTime.parse(args[4], inputFormatWithMS);
 
        //creating ramp JTL
        writer.println("timeStamp,elapsed,label,responseCode,responseMessage,threadName,dataType,success,failureMessage,bytes,sentBytes,grpThreads,allThreads,URL,Latency,IdleTime,Connect");
